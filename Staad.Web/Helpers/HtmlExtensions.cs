5bd355318c3acafec08dec6c6f373e9c05df2368
Roman Pavlushchenko <myromanmusic@gmail.com>
2014-06-08 10:04:20 +0400

    #6: bootstrap added to dictionaries

--- Staad.Web/Helpers/HtmlExtensions.cs
+++ Staad.Web/Helpers/HtmlExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Security.Policy;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Staad.Web.Helpers
 {
@@ -30,6 +34,30 @@ namespace Staad.Web.Helpers
 
             var mvcDiv = new MvcDiv(html.ViewContext, builder.Attributes["id"]);
             return mvcDiv;
+        }
+
+        public static MvcHtmlString Button(this HtmlHelper html, string buttonCaption, string actionName, string controllerName, object routeValues, object htmlAttributes)
+        {
+            if (string.IsNullOrEmpty(buttonCaption))
+            {
+                throw new ArgumentNullException("buttonCaption");
+            }
+
+            var rv = new RouteValueDictionary(routeValues);
+            var htmlAttrMap = (IDictionary<string, object>)HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+
+            var urlHelper = new UrlHelper(html.ViewContext.RequestContext);
+            var linkUrl = urlHelper.Action(actionName, controllerName, rv);
+
+            var tagBuilder = new TagBuilder("button")
+                {
+                    InnerHtml = !string.IsNullOrEmpty(buttonCaption) ? HttpUtility.HtmlEncode(buttonCaption) : string.Empty
+                };
+            tagBuilder.MergeAttributes(htmlAttrMap);
+            tagBuilder.MergeAttribute("href", linkUrl);
+
+            var btnSource = tagBuilder.ToString(TagRenderMode.Normal);
+            return MvcHtmlString.Create(btnSource);
         }
     }
 }
